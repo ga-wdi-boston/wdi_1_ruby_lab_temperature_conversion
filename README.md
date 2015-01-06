0de06f01fd7438529b7927794058aa1b913d2e4a
David Fisher <tibbon@gmail.com>
2015-01-06 17:10:54 -0500

    Solution for temperature conversion

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-wdi_1_ruby_lab_temperature_conversion
-=====================================
+![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
+
+# Lab: Temperature Conversion
+
+## Objectives
+
+By the end of this exercise a student should be able to:
+
+- Run RSpec tests
+- Write functions to fulfil those tests
+- Calculate results using math methods
+
+## Instructions
+
+## Bonus/Challenge
+
---
+++ lib/temperature_conversion.rb
@@ -0,0 +1,23 @@
+def fahrenheit_to_celsius(temperature)
+  (temperature - 32) / 1.8
+end
+
+def celsius_to_fahrenheit(temperature)
+  temperature * 1.8 + 32
+end
+
+def celsius_to_kelvin(temperature)
+  temperature + 273.15
+end
+
+def kelvin_to_celsius(temperature)
+  temperature - 273.15
+end
+
+def kelvin_to_fahrenheit(temperature)
+  (temperature - 273.15) * 1.8000 + 32.00
+end
+
+def fahrenheit_to_kelvin(temperature)
+  (temperature - 32) / 1.8 + 273.15
+end
