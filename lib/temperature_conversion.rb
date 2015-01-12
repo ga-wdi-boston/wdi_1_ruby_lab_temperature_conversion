8472c85f7f948b625543dc35aa89bd830795018a
Charles Talbot <charlietalbot@gmail.com>
2015-01-12 17:01:21 -0500

    Solved for Temp Translations

--- lib/temperature_conversion.rb
+++ lib/temperature_conversion.rb
@@ -2,4 +2,22 @@ def fahrenheit_to_celsius(temperature)
   (temperature - 32) / 1.8
 end
 
-# Your code goes here
+def celsius_to_fahrenheit(temperature)
+  temperature * 1.8 + 32
+end
+
+def celsius_to_kelvin(temperature)
+  temperature + 273.15
+end
+
+def kelvin_to_celsius(temperature)
+  temperature - 273.15
+end
+
+def kelvin_to_fahrenheit(temperature)
+  celsius_to_fahrenheit(kelvin_to_celsius(temperature))
+end
+
+def fahrenheit_to_kelvin(temperature)
+  celsius_to_kelvin(fahrenheit_to_celsius(temperature))
+end
