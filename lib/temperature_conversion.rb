d2903e3ff3e61071d66bce2c2ddecd2ab5435297
pat <patrick.h.needham@gmail.com>
2015-01-12 17:43:49 -0500

    convert temps

--- lib/temperature_conversion.rb
+++ lib/temperature_conversion.rb
@@ -1,5 +1,30 @@
 def fahrenheit_to_celsius(temperature)
-  (temperature - 32) / 1.8
+  celsius = (temperature - 32) / 1.8
+  celsius
 end
 
 # Your code goes here
+def celsius_to_fahrenheit(temperature)
+  fahrenheit = (temperature * 1.8) + 32
+  fahrenheit
+end
+
+def celsius_to_kelvin(temperature)
+  kelvin = temperature + 273.15
+  kelvin
+end
+
+def kelvin_to_celsius(temperature)
+  celsius = temperature - 273.15
+  celsius
+end
+
+def kelvin_to_fahrenheit(temperature)
+  celsius = temperature - 273.15
+  fahrenheit = (celsius * 1.8) + 32
+end
+
+def fahrenheit_to_kelvin(temperature)
+  celsius = (temperature - 32) / 1.8
+  kelvin = celsius + 273.15
+end
