fc24ed70a262063f0c67a7d5c8fa77144f8aa380
katyf <katy.feng.14@gmail.com>
2015-01-12 17:11:08 -0500

    Add F to K conversion.

--- lib/temperature_conversion.rb
+++ lib/temperature_conversion.rb
@@ -13,3 +13,11 @@ end
 def kelvin_to_celsius(temperature)
   temperature - 273.15
 end
+
+def kelvin_to_fahrenheit(temperature)
+  (temperature - 273.15) * 1.8 + 32
+end
+
+def fahrenheit_to_kelvin(temperature)
+  (temperature - 32) / 1.8 + 273.15
+end
