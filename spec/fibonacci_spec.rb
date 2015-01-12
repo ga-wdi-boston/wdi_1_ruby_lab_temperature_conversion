6f8de4c63d2a73eff0db3c3f6804496d3d8a7ef7
fishermanswharff <fishermanswharff@github.com>
2015-01-12 17:04:07 -0500

    fib spec

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -2,7 +2,7 @@ require './spec/spec_helper'
 require_relative '../lib/fibonacci'
 
 RSpec.describe "fibonacci_numbers" do
-  it 'returns the first x number of fibonacci numbers' do
-    expect(fibonacci_numbers(10)).to eq [1,1,2,3,5,8,13,21,34,55]
+  it 'returns the nth term of the fibonacci sequence' do
+    expect(fibonacci_numbers(10)).to eq 55
   end
 end
