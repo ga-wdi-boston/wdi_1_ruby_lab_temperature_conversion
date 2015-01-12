0131c4de9aa1e5eecae190431cc1a78769b18076
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-01-12 17:25:39 -0500

    Add temperature conversion lab

--- lib/temperature_conversion.rb
+++ lib/temperature_conversion.rb
@@ -2,4 +2,22 @@ def fahrenheit_to_celsius(temperature)
   (temperature - 32) / 1.8
 end
 
-# Your code goes here
+def celsius_to_fahrenheit(temperature)
+  temperature * 1.8 + 32
+end
+
+def celsius_to_kelvin(temperature)
+  temperature.to_f + 273.15
+end
+
+def kelvin_to_celsius(temperature)
+  temperature.to_f - 273.15
+end
+
+def kelvin_to_fahrenheit(temperature)
+  (temperature.to_f - 273.15) * 1.8 + 32
+end
+
+def fahrenheit_to_kelvin(temperature)
+  (temperature.to_f - 32) / 1.8 + 273.15
+end
