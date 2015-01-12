1e40d576529f044f2202b27adef134b741fda162
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-12 17:21:33 -0500

    Added different methods for temperature conversions

--- lib/temperature_conversion.rb
+++ lib/temperature_conversion.rb
@@ -3,3 +3,27 @@ def fahrenheit_to_celsius(temperature)
 end
 
 # Your code goes here
+
+def celsius_to_fahrenheit(temperature)
+  (temperature * 9/5) + 32
+end
+
+def celsius_to_kelvin(temperature)
+  temperature + 273.15
+
+ end
+
+def kelvin_to_celsius(temperature)
+    temperature - 273.15
+
+ end
+
+def kelvin_to_fahrenheit(temperature)
+    ((temperature - 273.15) * 1.8000) + 32
+
+end
+
+def fahrenheit_to_kelvin(temperature)
+    (temperature - 32) / 1.8000 + 273.15
+
+end
